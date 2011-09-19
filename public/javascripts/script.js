6f6003170ebb15e93e72ecdcb460b70c61be337e
Dave Lazar <dlazar@resistorsoftware.com>
2011-09-19 13:41:17 -0400

    bottom icons do the vertical slide thing

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,14 @@
 $(function(){
-  $('#my_slidedeck').slidedeck({hideSpines:true}).vertical();
+  
+  var deck = $('#my_slidedeck').slidedeck({hideSpines:true}).vertical();
+  console.log("deck ", deck);
+  
+  $("a.vertical_menu").click(function(e){
+    e.preventDefault();
+    var slide = this.href.replace(/.+#/,'');
+    $('#my_slidedeck').slidedeck({scroll:true}).vertical().goTo(slide);  
+  });
+
   $.fn.superfish.defaults.autoArrows = false;
   $('#main-nav').superfish({
     pathClass: 'current'
