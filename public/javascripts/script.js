5b7f35544946f642294dfc98b4864471fe34680b
Dave Lazar <dlazar@resistorsoftware.com>
2011-09-20 09:48:09 -0400

    more tweaks

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,24 +1,27 @@
 $(function(){
   
-  var mission = $('#team_mission').slidedeck({ hideSpines: true, keys: false, touch: false, cycle: true, speed: 1000, transition: "linear" });
-  $('#my_slidedeck').slidedeck({hideSpines:true}).vertical();
-  
-  $("a.vertical_menu").click(function(e){
-    e.preventDefault();
-    var slide = this.href.replace(/.+#/,'');
-    $('#my_slidedeck').slidedeck({scroll:true}).vertical().goTo(slide);  
-  });
-  
-  $('.inner_mission_nav .prev').click(function(e){
-    e.preventDefault();
-    mission.prev();
-  });
-  
-  $('.inner_mission_nav .next').click(function(e){
-    e.preventDefault();
-    mission.next();
-  });
+  if($('#team_mission').slidedeck() != null) {
+    var mission = $('#team_mission').slidedeck({ hideSpines: true, keys: false, touch: false, cycle: true, speed: 1000, transition: "linear" });
+    $('.inner_mission_nav .prev').click(function(e){
+      e.preventDefault();
+      mission.prev();
+    });
 
+    $('.inner_mission_nav .next').click(function(e){
+      e.preventDefault();
+      mission.next();
+    });
+  }
+  
+  if($('#my_slidedeck').length) {
+    $('#my_slidedeck').slidedeck({hideSpines:true}).vertical();
+    $("a.vertical_menu").click(function(e){
+      e.preventDefault();
+      var slide = this.href.replace(/.+#/,'');
+      $('#my_slidedeck').slidedeck({scroll:true}).vertical().goTo(slide);  
+    });
+  }
+  
   $.fn.superfish.defaults.autoArrows = false;
   $('#main-nav').superfish({
     pathClass: 'current'
