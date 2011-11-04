2bf6dcf952cafc0f7d7e7746522f9c23c11872f7
Dave Lazar <dlazar@resistorsoftware.com>
2011-11-04 14:27:50 -0400

    added the april template

--- public/javascripts/individual.js
+++ public/javascripts/individual.js
@@ -16,5 +16,24 @@ Vantage.individual = function () {
 
 
 $(function(){
+  $("#slideToggle").click(function () {
+    $('.slideTogglebox').slideDown('slow');
+  });
+  $("#slideToggle").hover(function () {
+    $(this).css("color","#F37421");
+  });
+  $("#slideToggle").mouseout(function () {
+    $(this).css("color","#455560");
+  });             
+  $("#clickme").click(function() {
+    $('.slideTogglebox').slideUp('slow'); 
+  });
+  $("#clickme").hover(function () {
+    $(this).css("color","#F37421");
+  });
+  $("#clickme").mouseout(function () {
+    $(this).css("color","#455560");
+  });
   
+  $("tr.stripe:even").addClass("even");             
 });
