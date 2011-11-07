c98b01acddfbb0036c5cd1b789e7f01427667bc5
Dave Lazar <dlazar@resistorsoftware.com>
2011-11-07 15:51:15 -0500

    fixed the top slidedeck of approach situation

--- public/javascripts/approach_situation.js
+++ public/javascripts/approach_situation.js
@@ -51,19 +51,14 @@ Vantage.situation = function () {
 
 $(function(){
   
-  var deck = $('#approach_slidedeck').slidedeck({hideSpines:true});
+  $("#teams_slidedeck").slidedeck({hideSpines: true}).vertical();
+  var deck = $('#approach_slidedeck').slidedeck({hideSpines: true});
   
   // these are clicks on the 4 bottom icons... so one of these will set the table for the current deck we want to control
   $("a.vertical_menu").click(function(e){
     e.preventDefault();
     var slide = this.href.replace(/.+#/,'');
-    // show/hide the controls here
-    if(slide == 1) {
-      $("#slidedeck_nav li.right").hide();
-    } else if (slide == 2) {
-      $("#slidedeck_nav li.right").show();
-    }
-    Vantage.utils.setCurrentDeck(slide);
+    $('#teams_slidedeck').slidedeck({scroll:true}).goTo(1).vertical().goTo(slide);
   });
  
   $("#slidedeck_mini .right .next").click(function() {
