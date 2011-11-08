e05edb172a2b4b8ae20231b6993cf8c8a69492c7
Dave Lazar <dlazar@resistorsoftware.com>
2011-11-08 12:03:14 -0500

    added a hover effect on method slides

--- public/javascripts/approach_method.js
+++ public/javascripts/approach_method.js
@@ -24,4 +24,10 @@ $(function(){
     $('#teams_slidedeck').slidedeck({scroll:true}).goTo(1).vertical().goTo(slide);
   });
   
+  $("#middle-content a").hover(function(e){
+    e.preventDefault();
+    var img = "images/"+this.href.replace(/.+#/,'')+".png";
+    $(this).parent().parent().attr("style", "background: white url("+img+") 0 0 no-repeat;");
+  })
+  
 });
