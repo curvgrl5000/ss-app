fffb3f505f73f63de9476911d70ff69987743f6b
Anna Mendoza <anna@curvecommunications.biz>
2012-05-03 03:34:31 -0700

    more updates on animations ....

--- public/javascripts/circulate.js
+++ public/javascripts/circulate.js
@@ -28,42 +28,78 @@ $(window).load(function() {
         }       
     });   
    
-
-    function startBallOne() {
+    function startInsectOne() {
         $("#butterfly-three").circulate({
             speed: 4000,
             height: 450,
             width: -650,
-            sizeAdjustment: 60,
+            sizeAdjustment: 85,
             loop: true,
             zIndexValues: [0, 0, 3, 3]
         });
     }
     
-    function startBallTwo() {
+    function startInsectTwo() {
         $("#butterfly-one").circulate({
             speed: 9000,
-            height: 520,
-            width: -1100,
-            sizeAdjustment: 25,
+            height: 820,
+            width: -1700,
+            sizeAdjustment: 75,
             loop: true,
             zIndexValues: [60, 60, 60, 60]
         })
     }
     
-    function startBallThree() {
+    function startInsectThree() {
         $("#butterfly-two").circulate({
             speed: 4000,
-            height: 300,
+            height: 700,
             width: -750,
-            sizeAdjustment: 10,
+            sizeAdjustment: 60,
             loop: true,
             zIndexValues: [3, 3, 1, 1]
         }).fadeIn();
     }
             
-    startBallOne();
-    setTimeout(startBallTwo, 1000);
-    setTimeout(startBallThree, 8000);
+    startInsectOne();
+    setTimeout(startInsectTwo, 1000);
+    setTimeout(startInsectThree, 8000);
+    
+    function startInsectFour() {
+        $("#butterfly-four").circulate({
+            speed: 5500,
+            height: -1800,
+            width: -650,
+            sizeAdjustment: 10,
+            loop: true,
+            zIndexValues: [60, 60, 60, 60]
+        });
+    }
+    
+    function startInsectFive() {
+        $("#butterfly-five").circulate({
+            speed: 5000,
+            height: 620,
+            width: -1500,
+            sizeAdjustment: 10,
+            loop: true,
+            zIndexValues: [50, 50, 50, 50]
+        })
+    }
+    
+    function startInsectSix() {
+        $("#butterfly-six").circulate({
+            speed: 6500,
+            height: 1000,
+            width: 3000,
+            sizeAdjustment: 80,
+            loop: true,
+            zIndexValues: [40, 40, 40, 40]
+        }).fadeIn();
+    }
+            
+    startInsectFour();
+    setTimeout(startInsectFive, 0);
+    setTimeout(startInsectSix, 0);
     
 });
